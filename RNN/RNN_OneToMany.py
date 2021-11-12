e55692af475365a1397bd5b5743d2bf26953eea7
unknown <equilibrio.top15@gmail.com>
2021-11-12 15:31:01 -0600

    -

---
+++ RNN/RNN_OneToMany.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 15:26:45 2021
+
+  This is a template of a RNN - one to many
+    Three RNN:
+        -RNN
+        -LSTM
+        -GRU 
+
+@author: Jorge Ivan Avalos Lopez
+python: 3.8.3
+pytorch: 1.6.0
+sklearn: 0.23.1
+"""
+
+import torch
+import torch.nn as nn
+from RNN-ManyToOne import RNN_MT1
+
