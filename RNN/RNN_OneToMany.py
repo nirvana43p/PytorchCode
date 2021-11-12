1e0811b8b178b5514a4a81f7c8bc807be6c62fd9
unknown <equilibrio.top15@gmail.com>
2021-11-12 15:53:33 -0600

    added RNN_OneToMany.py

--- RNN/RNN_OneToMany.py
+++ RNN/RNN_OneToMany.py
@@ -16,5 +16,46 @@ sklearn: 0.23.1
 
 import torch
 import torch.nn as nn
-from RNN-ManyToOne import RNN_MT1
+from RNN_ManyToOne import RNN_MT1
 
+
+class RNN_1TM(RNN_MT1):
+    
+    def __init__(self,input_dim, hidden_dim, num_layers, 
+                 batch_first = True, type_rnn = "RNN", **kwargs):
+        super(RNN_1TM,self).__init__(input_dim,input_dim,hidden_dim, num_layers, 
+                                     batch_first = True, type_rnn = "RNN", 
+                                     **kwargs)
+        
+    def forward(self, input_features, *args):
+        
+        if self._type_rnn in ("RNN","GRU"):
+            outputs, h_n = self.rnn(input_features,*args)
+        else:
+            outputs, (h_n,cn) = self.rnn(input_features,*args)
+        
+        # output of the h_n through the lineal 
+        output = self.lineal(outputs)
+            
+        
+        return output
+
+if __name__ == "__main__":
+    
+    # Input tensor
+    n_states = 5
+    batch_size = 128
+    input_dim = 4
+    
+    input_features = torch.randn(batch_size,n_states,input_dim)
+    print(input_features.shape)
+    
+    # RNN- one-To-Many
+    hidden_dim = 3
+    num_layers = 1
+    batch_first = True    
+    
+    model_rnn = RNN_1TM(input_dim, hidden_dim, num_layers, batch_first = True, type_rnn = "gru") 
+    
+    output = model_rnn(input_features)
+    print(output.shape)
